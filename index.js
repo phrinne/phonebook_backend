c93d1affa1b93b9566712deb6db731022ba18cb5
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-22 21:59:59 +0300

    Exercises 3.17 & 3.18

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :body',
     {
-        skip: function (req, res) { 
+        skip: function (req, res) {
             const { method, url } = req
-            return method !== 'POST' 
+            return method !== 'POST'
         }
     }
 ))
@@ -23,9 +23,9 @@ app.use(morgan(
 app.use(morgan(
     'tiny',
     {
-        skip: function (req, res) { 
+        skip: function (req, res) {
             const { method, url } = req
-            return method === 'POST' 
+            return method === 'POST'
         }
     }
 ))
@@ -49,19 +49,11 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
-            if(person) {
+            if (person) {
                 response.json(person)
             } else {
                 response.status(404).end()
-            }   
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
+            }
         })
         .catch(error => next(error))
 })
@@ -94,13 +86,33 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        //name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
+
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
     }
-  
+
     next(error)
 }
 app.use(errorHandler)
