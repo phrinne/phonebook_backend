8c0c260dc3cba5d6f45f418222a1c3fa1cc33204
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-22 21:39:43 +0300

    Exercise 3.15 & 3.16

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('body', (request, response) => JSON.stringify(request.body))
-
 //Morgan
 //POST using body token to show content of person
+morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :body',
     {
@@ -47,31 +46,29 @@ app.get('/api/persons', (request, response) => {
     Person.find({}).then(result => response.json(result))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
-            response.json(person)
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }   
         })
-        .catch((error) => {
-            console.log('error finding a person with id', request.params.id, error.message)
-            response.status(404).end()
-        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    /*const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)*/
-    console.log('delete pending mongoDB intetgration');
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 /*const nameExists = (name) => {
     const names = persons.map(p => p.name)
     return names.find(n => n === name)
-}*/
-/*const generateNewId = () => {
-    return Math.floor(Math.random() * 10000) + persons.length
 }*/
 
 app.post('/api/persons', (request, response) => {
@@ -96,6 +93,17 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
