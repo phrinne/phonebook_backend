53137bd82a0b50ad045538acb6f2b80efa0f77b8
phrinne <75909538+phrinne@users.noreply.github.com>
2021-04-23 19:38:53 +0300

    Exercise 3.20

--- index.js
+++ index.js
@@ -52,38 +52,24 @@ app.get('/api/persons/:id', (request, response, next) => {
             if (person) {
                 response.json(person)
             } else {
+                //person was already removed from server
                 response.status(404).end()
             }
         })
         .catch(error => next(error))
 })
 
-/*const nameExists = (name) => {
-    const names = persons.map(p => p.name)
-    return names.find(n => n === name)
-}*/
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if (!body.name) {
-        return response.status(400).json({ error: 'Name missing' })
-    }
-    if (!body.number) {
-        return response.status(400).json({ error: 'Number missing' })
-    }
-    /*if (nameExists(body.name)) {
-        return response.status(400).json({ error: 'name must be unique' })
-    }*/
-
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        console.log(`added ${savedPerson.name} number ${savedPerson.number} to phonebook`)
-        response.json(savedPerson)
-    })
+    person
+        .save()
+        .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -95,22 +81,28 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const person = {
-        //name: body.name,
-        number: body.number,
-    }
+    const person = { number: request.body.number}
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-        .then(updatedPerson => response.json(updatedPerson))
-        .catch(error => next(error))
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
+        .then(updatedPerson => {
+            if(updatedPerson) {
+                response.json(updatedPerson)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)
+        })
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.log(error.message)
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
